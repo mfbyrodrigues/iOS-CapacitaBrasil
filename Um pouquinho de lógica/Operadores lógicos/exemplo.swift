e86b7fe5c21cf716dc5406249f2c0ca50df2d94c
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 10:38:31 -0300

    Create exemplo.swift

---
+++ Um pouquinho de lógica/Operadores lógicos/exemplo.swift
@@ -0,0 +1,14 @@
+// Operadores lógicos
+var operando1 = false
+var operando2 = true
+var operando3 = true
+var operando4 = false
+
+print (operando1 && operando2) // AND
+print (operando2 && operando3) // AND
+
+print (operando1 || operando2) // OR
+print (operando1 || operando4) // OR
+
+print (!operando1) // NOT
+print (!operando2) // NOT
