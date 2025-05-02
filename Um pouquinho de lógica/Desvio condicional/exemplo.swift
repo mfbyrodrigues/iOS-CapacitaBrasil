023e93c1655043bf251a5fb5b6392ced6c46a3d3
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 10:43:55 -0300

    Create exemplo.swift

---
+++ Um pouquinho de lógica/Desvio condicional/exemplo.swift
@@ -0,0 +1,23 @@
+// Controle de fluxo: desvio condicional
+let n = 13
+
+if n < 13 {
+    print ("Menor!")
+} else if n > 13 {
+    print ("Maior!")
+} else {
+    print ("Igual!")
+}
+
+var cor = "vermelho"
+
+swith cor {
+    case "vermelho":
+        print ("Pare!")
+    case "amarelo":
+        print ("Espere!")
+    case "verde":
+        print ("Prossiga!")
+    default:
+        print ("Inválido!")
+}
