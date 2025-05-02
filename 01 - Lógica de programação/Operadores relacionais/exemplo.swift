e793d8217db2bb65952076bf53ab2512bdbc52c0
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 11:06:21 -0300

    Add files via upload

---
+++ 01 - Lógica de programação/Operadores relacionais/exemplo.swift
@@ -0,0 +1,10 @@
+// Operadores relacionais
+var a = 3
+var b = 7
+
+print ("a > b: \(a>b)")
+print ("a >= b: \(a>=b)")
+print ("a < b: \(a<b)")
+print ("a <= b: \(a<=b)")
+print ("a == b: \(a==b)")
+print ("a != b: \(a!=b)")
---
+++ 01 - Lógica de programação/Variáveis e constantes/exemplo.swift
@@ -0,0 +1,6 @@
+// Declaração de variáveis e constantes
+var idade = 13 // Variável
+let AnoDeNascimento = 2012 // Constante
+
+print (idade)
+print ("Ano de nascimento: \(AnoDeNascimento)") 
