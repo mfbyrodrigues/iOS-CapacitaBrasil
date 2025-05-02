c2173191f9d606fa47d1cee1305cc6241610ca2a
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 10:40:41 -0300

    Create exemplo.swift

---
+++ Um pouquinho de lógica/Escopo/exemplo.swift
@@ -0,0 +1,12 @@
+// Escopo
+func SomarDoisNumeros () {
+    let a = 1
+    let b = 2
+    
+    return a+b
+}
+
+SomarDoisNumeros()
+print (SomarDoisNumeros())
+let resultado = SomarDoisNumeros()
+print (resultado)
