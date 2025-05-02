724ee3317a02807f8184037b061c84f7b98f93f7
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 11:01:39 -0300

    Create exemplo.swift

---
+++ 01 - Lógica de programação/Operadores aritméticos/exemplo.swift
@@ -0,0 +1,17 @@
+// Operadores aritméticos
+
+print (10+3)
+
+let x = 10
+let y = 3
+let soma = x+y
+let subtracao = x-y
+let multiplicacao = x*y
+let divisao = x/y
+let resto = x % y
+
+print (soma)
+print (subtracao)
+print (multiplicacao)
+print (divisao)
+print (resto)
