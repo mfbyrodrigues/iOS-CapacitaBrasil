7e1d37a22c6721d9650a3f5a5297d6d86c03d0f9
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 10:39:18 -0300

    Create exemplo.swift

---
+++ Um pouquinho de lógica/Operadores de atribuição/exemplo.swift
@@ -0,0 +1,23 @@
+// Operadores de atribuição
+let constante = 3
+var variavel = 9
+
+variavel += 3 
+variavel = variavel + 3
+print (variavel)
+
+variavel -= 3
+variavel = variavel - 3
+print (variavel)
+
+variavel *= 3
+variavel = variavel * 3
+print (variavel)
+
+variavel /= 3
+variavel = variavel / 3
+print (variavel)
+
+variavel %= 3
+variavel = variavel % 3
+print (variavel)
