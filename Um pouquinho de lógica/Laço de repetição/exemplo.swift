9b971e4f624344f8832532b16b300987545150c3
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 10:43:08 -0300

    Create exemplo.swift

---
+++ Um pouquinho de lógica/Laço de repetição/exemplo.swift
@@ -0,0 +1,19 @@
+// Controle de fluxo: laço de repetição
+var numeros = [1, 2, 3]
+
+for numero in numeros {
+    print (numero)
+}
+
+iterador = 1
+while iterador <= 7 {
+    print (iterador)
+    
+    iterador += 1
+}
+
+repeat {
+    print (iterador)
+    
+    iterador += 1
+} while iterador <= 7
