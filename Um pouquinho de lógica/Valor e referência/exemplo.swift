c342098f5544f1a90f3536c90b78581338640702
M. Fernanda <n.nandcchi@gmail.com>
2025-05-02 11:05:10 -0300

    Delete Um pouquinho de lógica directory

--- Um pouquinho de lógica/Valor e referência/exemplo.swift
+++
@@ -1,3 +0,0 @@
-// Valor e referência
-// Referência: endereço do espaço reservado na memória
-// Valor: valor armazenado no espaço de memória
--- Um pouquinho de lógica/Variáveis e constantes/exemplo.swift
+++
@@ -1,6 +0,0 @@
-// Declaração de variáveis e constantes
-var idade = 13 // Variável
-let AnoDeNascimento = 2012 // Constante
-
-print (idade)
-print ("Ano de nascimento: \(AnoDeNascimento)") 
